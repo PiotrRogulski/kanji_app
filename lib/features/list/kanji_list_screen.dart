6e801c3d8881d5ad8450115bafc992e52dc4c454
Piotr Rogulski <piter.rogulski@gmail.com>
2025-04-14 22:48:25 +0200

    Implement readings list UI

--- lib/features/list/kanji_list_screen.dart
+++ lib/features/list/kanji_list_screen.dart
@@ -90,22 +90,49 @@ class _Entry extends StatelessWidget {
                     .copyWith(height: 1),
               ),
               if (entry.readings.onyomi.isNotEmpty)
-                Text(
-                  entry.readings.onyomi.join('\n'),
-                  style: theme.textTheme.labelLarge?.apply(
-                    color: theme.colorScheme.onSurface,
-                  ),
-                ),
+                _Readings(entry.readings.onyomi),
               if (entry.readings.kunyomi.isNotEmpty)
-                Text(
-                  entry.readings.kunyomi.join('\n'),
-                  style: theme.textTheme.labelLarge?.apply(
-                    color: theme.colorScheme.onSurface,
-                  ),
-                ),
+                _Readings(entry.readings.kunyomi),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _Readings extends StatelessWidget {
+  const _Readings(this.readings);
+
+  final List<String> readings;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return IntrinsicWidth(
+      child: Column(
+        spacing: 2,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          for (final (index, reading) in readings.indexed)
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.surface,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(index == 0 ? 8 : 4),
+                  bottom: Radius.circular(index == readings.length - 1 ? 8 : 4),
+                ),
+              ),
+              child: Text(
+                reading,
+                style: theme.textTheme.bodyLarge?.apply(
+                  color: theme.colorScheme.onSurface,
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
