f4286e1244364ca73adda1dd45cc764745737fe1
Piotr Rogulski <piter.rogulski@gmail.com>
2025-05-03 17:07:08 +0200

    Load up to 200th kanji

---
+++ lib/features/details/kanji_details_screen.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+// import 'package:kanji_app/features/kanji_data/kanji_data.dart';
+// import 'package:provider/provider.dart';
+//
+// class KanjiDetailsScreen extends StatelessWidget {
+//   const KanjiDetailsScreen(this.id, {super.key});
+//
+//   final int id;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: Add not found screen
+//     final kanji = context.read<KanjiData>().get(id)!;
+//   }
+// }
