3117efdd6de4f4e81f9dbc3deebbd6df153c4e27
Piotr Rogulski <piter.rogulski@gmail.com>
2025-05-10 14:39:26 +0200

    Setup logger, add logs to kanji loader

--- lib/features/kanji_data/loader.dart
+++ lib/features/kanji_data/loader.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:collection/collection.dart';
 import 'package:flutter/services.dart';
 import 'package:kanji_app/features/kanji_data/kanji_data.dart';
+import 'package:logging/logging.dart';
+
+Logger get _logger => Logger('KanjiLoader');
 
 Future<KanjiData> loadKanji() async {
   final assetsFile = await rootBundle.loadString('AssetManifest.json');
@@ -15,11 +18,35 @@ Future<KanjiData> loadKanji() async {
       .map(_loadKanji)
       .wait;
 
-  return KanjiData(data.sortedBy((e) => e.id));
+  final sortedData = data.nonNulls.sortedBy((e) => e.id);
+
+  _logger.fine('Loaded ${sortedData.length}/${data.length} kanji entries');
+
+  return KanjiData(sortedData);
 }
 
-Future<KanjiEntry> _loadKanji(String key) async {
-  final jsonString = await rootBundle.loadString(key);
-  final json = jsonDecode(jsonString) as Map<String, dynamic>;
-  return KanjiEntry.fromJson(json);
+Future<KanjiEntry?> _loadKanji(String key) async {
+  final String jsonString;
+  try {
+    jsonString = await rootBundle.loadString(key);
+  } catch (err, st) {
+    _logger.severe('Failed to load asset for $key', err, st);
+    return null;
+  }
+
+  final Map<String, dynamic> json;
+  try {
+    json = jsonDecode(jsonString) as Map<String, dynamic>;
+  } catch (err, st) {
+    _logger.severe('Failed to decode JSON for $key\n$jsonString', err, st);
+    return null;
+  }
+
+  try {
+    return KanjiEntry.fromJson(json);
+  } catch (err, st) {
+    final prettyJson = const JsonEncoder.withIndent('  ').convert(json);
+    _logger.severe('KanjiEntry.fromJson failed for $key\n$prettyJson', err, st);
+    return null;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kanji_app/features/kanji_data/loader.dart';
 import 'package:kanji_app/navigation/router.dart';
 import 'package:kanji_app/theme.dart';
 import 'package:leancode_hooks/leancode_hooks.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  _setupLogger();
 
   final kanjiData = await loadKanji();
 
@@ -22,4 +26,26 @@ class MainApp extends HookWidget {
 
     return MaterialApp.router(routerConfig: router, theme: appTheme());
   }
+}
+
+void _setupLogger() {
+  if (kDebugMode) {
+    Logger.root.level = Level.ALL;
+  } else {
+    Logger.root.level = Level.INFO;
+  }
+
+  Logger.root.onRecord.listen((record) {
+    final message = StringBuffer(
+      '[${record.loggerName}] ${record.level.name}: ${record.message}',
+    );
+    if (record.error case final error?) {
+      message.write('\nError: $error');
+    }
+    if (record.stackTrace case final stackTrace?) {
+      message.write('\nStackTrace: $stackTrace');
+    }
+
+    debugPrint(message.toString());
+  });
 }
