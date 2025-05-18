7f08b1fd72635b71863fd30b2f844d47d180591c
Piotr Rogulski <piter.rogulski@gmail.com>
2025-05-18 19:46:56 +0200

    Add wide layout

--- lib/features/details/kanji_details_screen.dart
+++ lib/features/details/kanji_details_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 import 'package:kanji_app/design_system.dart';
 import 'package:kanji_app/extensions.dart';
-import 'package:kanji_app/features/details/widgets/kanji_tile.dart';
-import 'package:kanji_app/features/details/widgets/readings_group.dart';
+import 'package:kanji_app/features/details/widgets/summary.dart';
 import 'package:kanji_app/features/details/widgets/words.dart';
 import 'package:kanji_app/features/kanji_data/kanji_data.dart';
 import 'package:provider/provider.dart';
@@ -16,24 +14,10 @@ class KanjiDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add not found screen
-    final kanji = context.read<KanjiData>().get(id)!;
-
-    return switch (Breakpoints.small.isActive(context)) {
-      true => _NarrowBody(kanji),
-      false => _WideBody(kanji),
-    };
-  }
-}
-
-class _NarrowBody extends StatelessWidget {
-  const _NarrowBody(this.entry);
-
-  final KanjiEntry entry;
-
-  @override
-  Widget build(BuildContext context) {
     final s = context.l10n;
+
+    // TODO: Add not found screen
+    final entry = context.read<KanjiData>().get(id)!;
 
     return Scaffold(
       body: CustomScrollView(
@@ -58,64 +42,32 @@ class _NarrowBody extends StatelessWidget {
           ),
           SliverPadding(
             padding: const AppPadding.all(AppUnit.large),
-            sliver: SliverMainAxisGroup(
-              slivers: [
-                SliverToBoxAdapter(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: AppUnit.small,
-                    children: [
-                      Wrap(
-                        spacing: AppUnit.small,
-                        runSpacing: AppUnit.small,
-                        children: [
-                          KanjiTile(entry),
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            spacing: AppUnit.small,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              GroupedKanjiRow(
-                                label: s.kanji_radical,
-                                items: [entry.radical],
-                              ),
-                              KanjiReadingsGroup(entry),
-                            ],
-                          ),
-                        ],
-                      ),
-                      if (entry.synonyms.isNotEmpty)
-                        GroupedKanjiRow(
-                          label: s.kanji_synonyms,
-                          items: entry.synonyms,
-                        ),
-                      if (entry.antonyms.isNotEmpty)
-                        GroupedKanjiRow(
-                          label: s.kanji_antonyms,
-                          items: entry.antonyms,
-                        ),
+            sliver: SliverLayoutBuilder(
+              builder: (context, constraints) {
+                return switch (constraints.crossAxisExtent) {
+                  < 700 => SliverMainAxisGroup(
+                    slivers: [
+                      SliverToBoxAdapter(child: KanjiSummary(entry: entry)),
+                      AppUnit.small.sliverGap,
+                      SliverKanjiWords(entry: entry),
                     ],
                   ),
-                ),
-                SliverKanjiWords(entry: entry),
-              ].spacedSliver(AppUnit.small),
+                  _ => SliverCrossAxisGroup(
+                    slivers: [
+                      SliverToBoxAdapter(child: KanjiSummary(entry: entry)),
+                      const SliverConstrainedCrossAxis(
+                        maxExtent: AppUnit.small,
+                        sliver: SliverToBoxAdapter(),
+                      ),
+                      SliverKanjiWords(entry: entry),
+                    ],
+                  ),
+                };
+              },
             ),
           ),
         ],
       ),
     );
-  }
-}
-
-class _WideBody extends StatelessWidget {
-  const _WideBody(this.kanji);
-
-  final KanjiEntry kanji;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement wide body
-    return _NarrowBody(kanji);
   }
 }
